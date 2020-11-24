249c750f70332b196fcf8b03a6e5c66a9b9d6403
Nyntofive <nyntofive@gmail.com>
2020-11-23 23:15:10 -0500

    Image Thumbnail signals

--- main/apps.py
+++ main/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class MainConfig(AppConfig):
     name = 'main'
+
+    # Register thumbnail signals with app @launch
+    def ready(self):
+        from . import signals
